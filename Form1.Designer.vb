bfffdebf67c3d07f44a88a30e836d83a573ab232
AllysonOliveiraSouza <131881163+AllysonOliveiraSouza@users.noreply.github.com>
2024-08-07 22:52:55 -0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtNomePasta = New TextBox()
+        Label1 = New Label()
+        btnCriarPasta = New Button()
+        SuspendLayout()
+        ' 
+        ' txtNomePasta
+        ' 
+        txtNomePasta.Location = New Point(106, 158)
+        txtNomePasta.Name = "txtNomePasta"
+        txtNomePasta.Size = New Size(548, 23)
+        txtNomePasta.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(291, 115)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(211, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Digite o nome da pasta"
+        ' 
+        ' btnCriarPasta
+        ' 
+        btnCriarPasta.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnCriarPasta.Location = New Point(320, 202)
+        btnCriarPasta.Name = "btnCriarPasta"
+        btnCriarPasta.Size = New Size(157, 58)
+        btnCriarPasta.TabIndex = 2
+        btnCriarPasta.Text = "Criar pasta"
+        btnCriarPasta.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnCriarPasta)
+        Controls.Add(Label1)
+        Controls.Add(txtNomePasta)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtNomePasta As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCriarPasta As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub btnCriarPasta_Click(sender As Object, e As EventArgs) Handles btnCriarPasta.Click
+
+        Dim nomePasta As String = txtNomePasta.Text
+        Dim caminhoDownloadUser = $"C:\Users\{Environment.UserName}\Downloads"
+        Dim caminhoCriacaoPasta = caminhoDownloadUser + "\" + nomePasta
+
+        AjustaCaminhoPasta(caminhoCriacaoPasta, caminhoDownloadUser, nomePasta)
+        CriarDirEmDownloads(caminhoCriacaoPasta)
+        MsgBox($"Pasta criada em {caminhoDownloadUser}")
+
+    End Sub
+
+    Private Sub CriarDirEmDownloads(caminhoCriacaoPasta As String)
+        Directory.CreateDirectory(caminhoCriacaoPasta)
+    End Sub
+
+    Private Sub AjustaCaminhoPasta(ByRef caminhoCriacaoPasta, ByRef caminhoDownloadUser, ByRef nomePasta)
+
+        Dim i As Integer = 1
+        While True
+
+            If Directory.Exists(caminhoCriacaoPasta) Then
+                caminhoCriacaoPasta = $"{caminhoDownloadUser}\{nomePasta}({i})"
+            Else
+                Exit While
+            End If
+
+            i += 1
+
+        End While
+
+    End Sub
+
+
+End Class
